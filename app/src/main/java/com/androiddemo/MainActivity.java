3925faef34472ac2b088f824a86cb1a682094c69
1466774522fxb <1578720182@qq.com>
2015-11-20 17:41:47 +0800

    library

--- app/src/main/java/com/androiddemo/MainActivity.java
+++ app/src/main/java/com/androiddemo/MainActivity.java
@@ -1,16 +1,18 @@
 package com.androiddemo;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
 
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
